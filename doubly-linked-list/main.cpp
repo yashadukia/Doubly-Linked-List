cfbcfe7ef75a019f4bae8d38c8e1773671086146
Yash Adukia <adukia.yash@gmail.com>
2016-03-19 22:42:40 -0700

    Initial Commit

---
+++ doubly-linked-list/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  doubly-linked-list
+//
+//  Created by Yash Adukia on 3/19/16.
+//  Copyright © 2016 Yash Adukia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
